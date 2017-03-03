44679630084d155158e78142f5435967c3542207
joeyshoop <joeyshoop@gmail.com>
2017-03-03 16:39:50 -0500

    Added old Bootstrap configuration

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,10 @@
 Rails.application.routes.draw do
   get 'static_pages/home'
-
   get 'static_pages/about'
-
   get 'static_pages/contact'
-
   get 'static_pages/reasonsToOppose'
-
   get 'static_pages/whatCanBeDone'
+  root 'application#hello'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'application#hello'
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,29 +1,39 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @base_title = "No Convention of States North Carolina"
+  end
+  
   test "should get home" do
     get static_pages_home_url
     assert_response :success
+    assert_select "title", "Home | #{@base_title}"
   end
 
   test "should get about" do
     get static_pages_about_url
     assert_response :success
+    assert_select "title", "About | #{@base_title}"
   end
 
   test "should get contact" do
     get static_pages_contact_url
     assert_response :success
+    assert_select "title", "Contact | #{@base_title}"
   end
 
   test "should get reasonsToOppose" do
     get static_pages_reasonsToOppose_url
     assert_response :success
+    assert_select "title", "Reasons To Oppose | #{@base_title}"
   end
 
   test "should get whatCanBeDone" do
     get static_pages_whatCanBeDone_url
     assert_response :success
+    assert_select "title", "What Can Be Done | #{@base_title}"
   end
 
 end
