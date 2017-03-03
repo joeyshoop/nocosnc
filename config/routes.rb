5bad4e48ac778d608c1bfeadd48f799e4e58aea4
joeyshoop <joeyshoop@gmail.com>
2017-03-03 17:23:12 -0500

    Fixed links in header

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
-  get 'static_pages/about'
-  get 'static_pages/contact'
-  get 'static_pages/reasonsToOppose'
-  get 'static_pages/whatCanBeDone'
+  get '/about', to: 'static_pages#about'
+  get '/contact', to: 'static_pages#contact'
+  get '/reasonsToOppose', to: 'static_pages#reasonsToOppose'
+  get '/whatCanBeDone', to: 'static_pages#whatCanBeDone'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
