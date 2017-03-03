2820918415585fd37bb803768d5a06346c208700
joeyshoop <joeyshoop@gmail.com>
2017-03-03 16:53:09 -0500

    Added Image file

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
+  root 'static_pages#home'
   get 'static_pages/about'
   get 'static_pages/contact'
   get 'static_pages/reasonsToOppose'
   get 'static_pages/whatCanBeDone'
-  root 'application#hello'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
